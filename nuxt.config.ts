7e3ef39520f0f801bad26ab594fb87cc7d59312b
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-10-16 00:37:34 +0200

    ssr fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+
+var ssr = (process.env.NUXT_SSR === 'true');
+
 export default defineNuxtConfig({
   modules: ["nuxt-directus",'@nuxtjs/tailwindcss','@nuxt/content', '@pinia/nuxt','nuxt-icon'],
+  ssr: ssr,
   directus: {
     url: process.env.NUXT_API_URL
   },
