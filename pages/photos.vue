56a9d04a1de594b4c379e5413242882071330cac
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-10-19 10:24:21 +0200

    Update photos.vue

--- pages/photos.vue
+++ pages/photos.vue
@@ -1,23 +1,11 @@
 <script setup lang="ts">
-
-if(process.client) {
-  let hero = document.getElementById('photo-page-hero')
-  let els = document.getElementsByTagName('nav')
-  let h = 0
-  if(els && els.length != 0) {
-    const nav = els[0] as HTMLElement
-    h = nav.clientHeight
-  }
-}
-
-
 </script>
 
 <template>
   <div>
     <ClientOnly>
     <Teleport to="#hero-teleport">
-      <div id="photo-page-hero" class="hero min-w-screen min-h-full" style="background-image: url(/assets/img/19.jpg);">
+      <div id="photo-page-hero" class="hero min-w-screen min-h-full" style="background-image: url(https://cms.fredrikburmester.com/assets/bce28360-a8e1-48fd-93d7-5089ab9244e2);">
         <div class="hero-overlay bg-opacity-60"></div>
         <div class="hero-content text-center text-neutral-content">
           <div class="max-w-md">
